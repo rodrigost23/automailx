c2fc4c353eb033a695f388416e8b27dee69eb01f
Rodrigo Tavares <rodrigost23@gmail.com>
2019-03-16 13:56:48 -0400

    Starting classifier (WIP)

---
+++ clf_models.py
@@ -0,0 +1,146 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import model_selection, svm
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.ensemble import (AdaBoostClassifier, ExtraTreesClassifier,
+                              GradientBoostingClassifier,
+                              RandomForestClassifier)
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import learning_curve
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+# from sklearn.metrics import explained_variance_score, make_scorer
+
+seed = 100
+
+X = []
+Y = []
+for filename in os.listdir('data'):
+    data = pd.read_csv('data/'+filename, sep=',', header=None, index_col=0)
+    # print(data)
+    for i in data.index.unique():
+        # Index is activity ID
+        # Y.append(i)
+        # X.append(data.loc[i].values[:250]) #TODO: Change this maximum value to some better dataset
+        for coords in data.loc[i].values:
+            Y.append(i)
+            X.append(coords)
+
+# prepare models
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+models.append(('ADB', AdaBoostClassifier()))
+models.append(('RFC', RandomForestClassifier()))
+models.append(('ETC', ExtraTreesClassifier()))
+models.append(('GBC', GradientBoostingClassifier()))
+
+"""
+X_train, X_test, y_train, y_test = train_test_split(X, Y,
+test_size=0.3,
+random_state=100)
+"""
+
+# evaluate each model in turn
+results = []
+names = []
+cv_means = []
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+scoring = 'accuracy'
+for name, model in models:
+    cv_results = model_selection.cross_val_score(model, X, Y,
+                                                 cv=kfold,
+                                                 scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    cv_means.append(cv_results.mean())
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+
+
+"""
+learning curve for best results from models
+"""
+# size_data = len(X)
+# print(size_data)
+# cv_fold = model_selection.KFold(size_data, shuffle=True)
+
+call_models = dict(models)
+cfl = call_models[names[pd.Series(cv_means).idxmax()]]
+# cfl = AdaBoostClassifier()
+train_sizes, train_scores, test_scores = learning_curve(cfl,
+                                                        X, Y,
+                                                        n_jobs=-1,
+                                                        cv=kfold,
+                                                        train_sizes=np.linspace
+                                                        (.1, 1.0, 5),
+                                                        verbose=0)
+
+train_scores_mean = np.mean(train_scores, axis=1)
+train_scores_std = np.std(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+test_scores_std = np.std(test_scores, axis=1)
+
+plt.figure()
+plt.title(names[pd.Series(cv_means).idxmax()])
+plt.legend(loc="best")
+plt.xlabel("Training examples")
+plt.ylabel("Score")
+plt.gca().invert_yaxis()
+
+# box-like grid
+plt.grid()
+
+# plot the std deviation as a transparent range at each training set size
+plt.fill_between(train_sizes,
+                 train_scores_mean - train_scores_std,
+                 train_scores_mean + train_scores_std,
+                 alpha=0.1,
+                 color="r")
+
+plt.fill_between(train_sizes,
+                 test_scores_mean - test_scores_std,
+                 test_scores_mean + test_scores_std,
+                 alpha=0.1,
+                 color="g")
+
+# plot the average training and test score lines at each training set size
+plt.plot(train_sizes,
+         train_scores_mean,
+         'o-',
+         color="r",
+         label="Training score")
+
+plt.plot(train_sizes,
+         test_scores_mean,
+         'o-',
+         color="g",
+         label="Cross-validation score")
+
+# sizes the window for readability and displays the plot
+# shows error from 0 to 1.1
+plt.ylim(-.1, 1.1)
+# plt.xlabel("Training Set Size"), plt.ylabel("Accuracy Score"),
+plt.legend(loc="best")
+plt.show()
+
+"""
+boxplot algorithm comparison
+"""
+
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
---
+++ clf_predict.py
@@ -0,0 +1,91 @@
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from sklearn.metrics import classification_report, \
+#        confusion_matrix, \
+#        accuracy_score
+import os
+import time
+from collections import deque
+
+import numpy as np
+import pandas as pd
+from sklearn import model_selection
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+import sensors
+
+# prepare configuration for cross validation test harness
+seed = 100
+
+X = []
+Y = []
+for filename in os.listdir('data'):
+    data = pd.read_csv('data/'+filename, sep=',', header=None, index_col=0)
+    # print(data)
+    for i in data.index.unique():
+        # Index is activity ID
+        # Y.append(i)
+        # X.append(data.loc[i].values[:250]) #TODO: Change this maximum value to some better dataset
+        for coords in data.loc[i].values:
+            Y.append(i)
+            X.append(coords)
+
+# Split data
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    X, Y,
+    test_size=0.3,
+    random_state=100)
+
+# classifier
+clf = GradientBoostingClassifier()
+
+kfold = model_selection.KFold(n_splits=10, random_state=seed)
+scoring = 'accuracy'
+
+cv_results = model_selection.cross_val_score(clf, X, Y,
+                                             cv=kfold,
+                                             scoring=scoring)
+
+clf.fit(X_train, y_train)
+
+# Xnew = [[7380, 204, 14644, 69, -117, 191]]
+s = sensors.Sensors()
+start_time = None
+elapsed_time = None
+dq = deque()
+
+try:
+    while True:
+        data = s.read()
+        if data is None:
+            continue
+        elif start_time is None:
+            start_time = time.time()
+        elapsed_time = time.time() - start_time
+
+        print("\r", end='')
+        if elapsed_time is not None and start_time is not None:
+            print("[%ds] " % (elapsed_time), end='')
+
+        print(str(data) + " " * 5, end='')
+        dq.append(
+            {'time': elapsed_time, 'data': sensors.SensorData(*data.data())})
+        # if time more than 1 second
+        if dq and elapsed_time - dq[0]['time'] >= 1:
+            save_data = data - dq.popleft()['data']
+
+            # data_tobe_classified = pd.read_csv('new_data.csv',
+            #                                 sep=',',
+            #                                 header=None)
+
+            y_pred = clf.predict(np.asarray([(list(save_data.data()))]))
+
+            print("        Prediction: %s" % (y_pred) + " " * 5, end='')
+
+    print("\r[%ds] " % (elapsed_time), end='')
+
+    i += 1
+
+except KeyboardInterrupt:
+    print("Interrupted." + " " * 36)
