33e159b32b78967847547e03ab4ec2976f02170c
Rodrigo Tavares <rodrigost23@gmail.com>
2019-05-16 17:57:36 -0400

    Move classifier initialization to class constructor

--- clf_predict.py
+++ clf_predict.py
@@ -12,45 +12,36 @@ from sklearn.tree import DecisionTreeClassifier
 import sensors
 
 class Predict():
-    # prepare configuration for cross validation test harness
-    seed = 100
-
-    X = []
-    Y = []
-    for filename in os.listdir('data'):
-        data = pd.read_csv('data/'+filename, sep=',', header=None, index_col=0)
-        # print(data)
-        for i in data.index.unique():
-            # Index is activity ID
-            # Y.append(i)
-            # X.append(data.loc[i].values[:250]) #TODO: Change this maximum value to some better dataset
-            for coords in data.loc[i].values:
-                Y.append(i)
-                X.append(coords)
-
-    # Split data
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(
-        X, Y,
-        test_size=0.3,
-        random_state=100)
-
-    # classifier
-    clf = DecisionTreeClassifier()
-
-    kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    scoring = 'accuracy'
-
-    cv_results = model_selection.cross_val_score(clf, X, Y,
-                                                cv=kfold,
-                                                scoring=scoring)
-
-    clf.fit(X_train, y_train)
 
     # Xnew = [[7380, 204, 14644, 69, -117, 191]]
     start_time = None
     elapsed_time = None
     dq = deque()
+    # classifier
+    clf = DecisionTreeClassifier()
 
+    def __init__(self):
+
+        X = []
+        Y = []
+        for filename in os.listdir('data'):
+            data = pd.read_csv('data/'+filename, sep=',', header=None, index_col=0)
+            # print(data)
+            for i in data.index.unique():
+                # Index is activity ID
+                # Y.append(i)
+                # X.append(data.loc[i].values[:250]) #TODO: Change this maximum value to some better dataset
+                for coords in data.loc[i].values:
+                    Y.append(i)
+                    X.append(coords)
+
+        # Split data
+        X_train, X_test, y_train, y_test = model_selection.train_test_split(
+            X, Y,
+            test_size=0.3,
+            random_state=100)
+
+        self.clf.fit(X_train, y_train)
 
     def predict(self, data: sensors.SensorData):
         if data is None:
